8cc20001e7e29b0011d732f5440f5cbe20880934
zakaria <zakariaazaraf@gmail.com>
2022-01-23 18:04:46 +0100

    Added the pages logo, implemente the breakpoint to change the display mode

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material'
 import { makeStyles } from '@mui/styles'
 import  SendIcon  from '@mui/icons-material/Send'
+import Navbar from './components/Navbar'
 
 // The theme is imported in the index page and rappered inside 
 const useStyles = makeStyles((theme)=> ({
@@ -15,20 +16,7 @@ const App = () => {
   console.log(classes)
   return (
     <div className="App">
-      <header className="App-header">
-      
-      </header>
-      <Button className={classes.button} variant="outlined" startIcon={<SendIcon />}>Contained</Button>
-      <Button className={classes.button} variant="contained" disabled>
-        Disabled
-      </Button>
-      <Button className={classes.button} variant="outlined" href="#contained-buttons" endIcon={<SendIcon />}>
-        Link
-      </Button>
-
-      <Button className={classes.button} variant="contained">
-        Link
-      </Button>
+        <Navbar />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,31 @@
 import React from 'react';
+import { makeStyles } from '@mui/styles'
+import { AppBar, Toolbar, Typography } from '@mui/material';
 
-export const Navbar = () => {
-  return <div></div>;
+
+const useStyles = makeStyles((theme) => ({
+  logoSM: {
+    display: 'none',
+    [theme.breakpoints.down('sm')] : {
+      display: 'block'
+    }
+  },
+  logoLG: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block'
+    }
+  }
+}))
+
+const Navbar = () => {
+  const classes = useStyles()
+  return <AppBar>
+            <Toolbar>
+              <Typography variant='h5' component='a' className={classes.logoLG}>Logo LG</Typography>
+              <Typography variant='h6' component='a' className={classes.logoSM}>Logo SM</Typography>
+            </Toolbar>
+        </AppBar>;
 };
+
+export default Navbar
