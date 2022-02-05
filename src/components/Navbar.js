d9e4b36e9cb9ced36042ab851c63380455a1c4cb
zakaria <zakariaazaraf@gmail.com>
2022-02-05 18:52:38 +0100

    Change the serach placeholder dynamicaly on small screens, used the props to chnage styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { makeStyles } from '@mui/styles'
 import { AppBar, InputBase, Toolbar, Typography, alpha, IconButton } from '@mui/material';
 import { Search } from '@material-ui/icons';
@@ -10,44 +10,57 @@ import { Avatar } from '@mui/material';
 
 
 const useStyles = makeStyles((theme) => ({
+
   toolbar: {
     dispaly: 'flex',
     justifyContent: 'space-between',
     flexWrap: 'wrap',
   },
+
   logoSM: {
     display: 'none',
     [theme.breakpoints.down('sm')] : {
       display: 'block'
     }
   },
+
   logoLG: {
     display: 'none',
     [theme.breakpoints.up('sm')]: {
       display: 'block'
     }
   },
+
   search: {
     display: 'flex',
     alignItems: 'center',
+    flexGrow: '0.4',
     backgroundColor: alpha(theme.palette.common.white, 0.15),
     '&:hover': {
       backgroundColor: alpha(theme.palette.common.white, 0.25),
     },
     borderRadius: theme.shape.borderRadius,
-    width: '40%',
     [theme.breakpoints.down('sm')]: {
-      display: 'none'
-    }
+      display: (props) => (props.open ? 'flex' : 'none'),
+    },
+    [theme.breakpoints.up('sm')]: {
+      display: 'flex',
+    },
   },
+
   input: {
     color: '#FFF',
     marginLeft: theme.spacing(1.5)
   },
+
   icons: {
     display: 'flex',
-    alignItems: 'center'
+    alignItems: 'center',
+    [theme.breakpoints.down('sm')]: {
+      display: (props) => (props.open ? 'none' : 'flex'),
+    },
   },
+
   avatar: {
     [theme.breakpoints.up('sm')]: {
       width: '32px',
@@ -57,48 +70,62 @@ const useStyles = makeStyles((theme) => ({
       width: '28px',
       height: '28px',
     },
+  },
+
+  searchSmIcon: {
+    [theme.breakpoints.up('sm')]: {
+      display: 'none'
+    },
+    [theme.breakpoints.down('sm')]: {
+      display: 'block'
+    }
   }
 }))
 
 const Navbar = () => {
-  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+  const classes = useStyles({ open })
+  
   return <AppBar>
             <Toolbar className={classes.toolbar}>
               <Typography variant='h5' component='a' className={classes.logoLG}>Logo LG</Typography>
               <Typography variant='h6' component='a' className={classes.logoSM}>Logo SM</Typography>
               <div className={classes.search}>
                 <Search />
-                <InputBase placeholder='Search' className={classes.input}/>
+                <InputBase placeholder='Search' className={classes.input} fullWidth/>
               </div>
-              <Search />
-              <div className={classes.icons}>
-                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
-                  <Badge badgeContent={5} color="error">
-                    <MailIcon />
-                  </Badge>
-              </IconButton>
-              <IconButton
-                size="large"
-                aria-label="show 17 new notifications"
-                color="inherit"
-              >
-                <Badge badgeContent={17} color="error">
-                  <NotificationsIcon />
-                </Badge>
-              </IconButton>
+              <Search className={classes.searchSmIcon} onClick={()=>{setOpen(!open)}}/>
 
-              {/* <IconButton
-                size="large"
-                aria-label="account of current user"
-                aria-controls="primary-search-account-menu"
-                aria-haspopup="true"
-                color="inherit"
-              >
-                <AccountCircle />
-              </IconButton> */}
-              <IconButton>
-                <Avatar alt="Remy Sharp" src="/images/avatarProfile.jpg" className={classes.avatar}/>
-              </IconButton>
+              <div className={classes.icons}>
+
+                <IconButton size="large" aria-label="show 4 new mails" color="inherit">
+                    <Badge badgeContent={5} color="error">
+                      <MailIcon />
+                    </Badge>
+                </IconButton>
+
+                <IconButton
+                  size="large"
+                  aria-label="show 17 new notifications"
+                  color="inherit"
+                >
+                  <Badge badgeContent={17} color="error">
+                    <NotificationsIcon />
+                  </Badge>
+                </IconButton>
+
+                {/* <IconButton
+                  size="large"
+                  aria-label="account of current user"
+                  aria-controls="primary-search-account-menu"
+                  aria-haspopup="true"
+                  color="inherit"
+                >
+                  <AccountCircle />
+                </IconButton> */}
+                <IconButton>
+                  <Avatar alt="Remy Sharp" src="/images/avatarProfile.jpg" className={classes.avatar}/>
+                </IconButton>
               </div>
             </Toolbar>
         </AppBar>;
